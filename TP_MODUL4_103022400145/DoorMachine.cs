6c722aa67ed90068dbea83627b1c39dc9d58f26f
M. Reza Saputra <email_github_monmantul@gmail.com>
2026-03-16 01:33:59 +0700

    menambahkan class DoorMachine

---
+++ TP_MODUL4_103022400145/DoorMachine.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace TP_MODUL4_103022400145
+{
+    public class DoorMachine
+    {
+        public enum State { Terkunci, Terbuka };
+        public State currentState;
+
+        public DoorMachine()
+        {
+            currentState = State.Terkunci;
+            Console.WriteLine("Pintu terkunci");
+        }
+
+        public void PerubahanState(string perintah)
+        {
+            if (currentState == State.Terkunci && perintah == "BukaPintu")
+            {
+                currentState = State.Terbuka;
+                Console.WriteLine("Pintu tidak terkunci");
+            }
+            else if (currentState == State.Terbuka && perintah == "KunciPintu")
+            {
+                currentState = State.Terkunci;
+                Console.WriteLine("Pintu terkunci");
+            }
+        }
+    }
+}
--- TP_MODUL4_103022400145/Program.cs
+++ TP_MODUL4_103022400145/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace TP_MODUL4_103022400145
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("--- OUTPUT BAGIAN C: KODE POS ---");
+            string kelurahan = "Batununggal";
+            Console.WriteLine("Kelurahan: " + kelurahan + " | Kode Pos: " + KodePos.getKodePos(kelurahan));
+            Console.WriteLine();
+
+            Console.WriteLine("--- OUTPUT BAGIAN E: DOOR MACHINE ---");
+            DoorMachine pintu = new DoorMachine();
+            pintu.PerubahanState("BukaPintu"); 
+            pintu.PerubahanState("KunciPintu"); 
+
+            Console.WriteLine("\nTekan Enter untuk keluar...");
+            Console.ReadLine();
+        }
+    }
+}
